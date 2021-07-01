8411f3da6cf8fffd98aaed17e922a0b5909acfe4
Traindriver-M <sergioparovozov@mail.ru>
2021-07-01 16:53:10 +0300

    4.1. Операторы ветвлений [#3611 #173333]

--- src/ru/job4j/condition/AlertDivByZero.java
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -2,12 +2,15 @@ package ru.job4j.condition;
 
 public class AlertDivByZero {
     public static void main(String[] args) {
-        possibleDiv(-4);
+        possibleDiv(4);
         possibleDiv(0);
 
     }
 
     static void possibleDiv(int number) {
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
         if (number < 0) {
             System.out.println("This is negative numbers");
         }
