22025cb2d1059fce554c5f8fb7d9d2571a77e24b
Traindriver-M <sergioparovozov@mail.ru>
2021-07-08 23:41:36 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Павлик Корчагин";
+        names[1] = "Аркадий Гайдар";
+        names[2] = "Зина Портнова";
+        names[3] = "Валя Котик";
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
